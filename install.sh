f61d0ba84154f13e0d54052e2a26c8a47ba8709e
Joshua Ball <joshball@yahoo-inc.com>
2014-12-31 05:55:52 +0000

    more installs

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 set -ev
 
-sudo apt-get install vim
-sudo apt-get install zsh
+sudo apt-get update
+sudo apt-get install vim zsh curl
 curl -L http://install.ohmyz.sh | sh
+sudo chsh -s /usr/bin/zsh vagrant
 ./bin/copy-from-repo.sh
 git config --global user.name "Joshua Ball"
 git config --global user.email joshbball@gmail.com
