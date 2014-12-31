a47b01c83db2a88116271d2a862d0f6fa8827a00
Joshua Ball <joshbball@gmail.com>
2014-12-31 05:38:29 +0000

    git name

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ set -ev
 sudo apt-get install zsh
 curl -L http://install.ohmyz.sh | sh
 ./bin/copy-from-repo.sh
+git config --global user.name "Joshua Ball"
+git config --global user.email joshbball@gmail.com
